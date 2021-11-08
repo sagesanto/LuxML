63fe4729c867a8d5350fac5c1bb4db4c0b0b0feb
sagesanto <chellster1003@gmail.com>
2021-11-08 11:00:21 -0500

    need to score it a lot better goddamnit

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 from typing import Any
 import json
+from json import JSONEncoder
 import numpy as np
 import pandas as pd
-import sklearn
+import scipy
 import matplotlib
 import base64
 import math, sys
 import subprocess
 import shlex
 import pygame
+from scipy.optimize import minimize, rosen, rosen_der
 
 
 def readJson(dump):
     enc = json.loads(dump)
     dataArray = enc
-    print("data array in LuxML:",dataArray)
+    # print("data array in LuxML:", dataArray)
     return dataArray
 
 
@@ -25,38 +27,52 @@ def readFromFile(filename):
 
 
 def getResults():
-    encResults = readFromFile("C:/LuxAI/rexai/gameDataAndResults.txt")
+    encResults = readFromFile("C:/LuxAI/rexai/scores.txt")
     score = readJson(encResults)
     return score
 
 
+class NumpyArrayEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return JSONEncoder.default(self, obj)
+
 def encodeJson(inputLists):
-    nparray = np.array([inputLists], dtype=object)
-    print("encode:",base64.b64encode(nparray).decode('utf-8'))
-    return json.dumps([str(nparray.dtype), nparray.tolist(), nparray.shape])
+    # nparray = {"array":nparray}
+    # print(type(inputLists))
+    # print(inputLists.dtype)
+    # print(inputLists.shape)
+    return json.dumps(inputLists, cls=NumpyArrayEncoder)
+# [str(nparray.dtype), nparray.to_list(), nparray.shape]
 
 
 def writeToFile(filename, string):
     with open(filename, 'a') as f:
-        f.write(string+ '\n')
+        f.write(string + '\n')
 
 
 def writeInstructions(inputLists):
     inputString = encodeJson(inputLists)
-    print(inputString)
-    writeToFile("C:/LuxAI/rexai/gameDataAndResults.txt", inputString)
+    print("inputString from write:", inputString)
+    writeToFile("C:/LuxAI/rexai/instructions.txt", inputString)
 
 
 def runGame(inputLists):
     writeInstructions(inputLists)
     command = "C:/Users/Student/AppData/Roaming/npm/lux-ai-2021.cmd C:/LuxAI/simple/main.py C:/LuxAI/rexAI/main.py --maxtime 3000 --out replay.json"
     args = shlex.split(command)
-    print(args)
+    # print(args)
     proc = subprocess.Popen(args)
     while proc.returncode is None:
         proc.poll()
-    return getResults()
+    return -getResults()
 
 
-score = runGame([[1, 2, 3, 4, 5], [1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5], [1, 2, 3, 4, 5]])
-print("score:",score)
+array = np.array([[1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6]])
+# print(type(array))
+# print(array.shape)
+# print("array:", array)
+res = scipy.optimize.minimize(runGame, array)
+print("result:", res.x, "success:", res.success, "message:", res.message, "iterations:", res.iter, "lastScore:",
+      getResults())
