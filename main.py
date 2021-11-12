c1926028932a9ff09df89795a4e151877a516c70
sagesanto <chellster1003@gmail.com>
2021-11-12 11:13:44 -0500

    ML program switched to use COBYLA. investigate CMA-ES with optuna.

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ import shlex
 import pygame
 from scipy.optimize import minimize, rosen, rosen_der
 
+summaryFrame = pd.DataFrame()
+batchNumber = 0
+
 
 def readJson(dump):
     enc = json.loads(dump)
@@ -38,18 +41,26 @@ class NumpyArrayEncoder(JSONEncoder):
             return obj.tolist()
         return JSONEncoder.default(self, obj)
 
+
 def encodeJson(inputLists):
     # nparray = {"array":nparray}
     # print(type(inputLists))
     # print(inputLists.dtype)
     # print(inputLists.shape)
     return json.dumps(inputLists, cls=NumpyArrayEncoder)
+
+
 # [str(nparray.dtype), nparray.to_list(), nparray.shape]
 
 
 def writeToFile(filename, string):
     with open(filename, 'a') as f:
         f.write(string + '\n')
+
+
+def overwriteFile(filename, string):
+    with open(filename, 'w') as f:
+        f.write(string)
 
 
 def writeInstructions(inputLists):
@@ -59,20 +70,38 @@ def writeInstructions(inputLists):
 
 
 def runGame(inputLists):
+    global batchNumber
+    global summaryFrame
+    batchNumber += 1
     writeInstructions(inputLists)
     command = "C:/Users/Student/AppData/Roaming/npm/lux-ai-2021.cmd C:/LuxAI/simple/main.py C:/LuxAI/rexAI/main.py --maxtime 3000 --out replay.json"
     args = shlex.split(command)
-    # print(args)
-    proc = subprocess.Popen(args)
-    while proc.returncode is None:
-        proc.poll()
-    return -getResults()
+    score = 0
+    gamesPerBatch = 20
+    for i in range(gamesPerBatch):
+        proc = subprocess.Popen(args)
+        while proc.returncode is None:
+            proc.poll()
+        score = score - getResults()
+    summaryFrame = summaryFrame.append(
+        {"batch": batchNumber, "averageScore": score / gamesPerBatch, "parameters": inputLists}, ignore_index=True)
+
+    overwriteFile("C:/Users/Student/PycharmProjects/LuxML/summary.csv", str(summaryFrame.to_csv()))
+    print(summaryFrame)
+    return score / gamesPerBatch
 
 
-array = np.array([[1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6]])
-# print(type(array))
-# print(array.shape)
-# print("array:", array)
-res = scipy.optimize.minimize(runGame, array)
-print("result:", res.x, "success:", res.success, "message:", res.message, "iterations:", res.iter, "lastScore:",
-      getResults())
+array = np.array([[1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6]], dtype="int64")
+
+
+def con(x):
+    return max[x - int(x)] == 0
+
+
+cons = ({'type': 'eq', 'fun': lambda x: max([x[i] - int(x[i]) for i in range(len(x))])})
+res = scipy.optimize.minimize(runGame, array, method="COBYLA", options = {"rhobeg" : 5})
+output = str(
+    "result:" + res.x + "success:" + res.success + "message:" + res.message + "iterations:" + res.nit + "lastScore:" +
+    getResults())
+writeToFile("C:/LuxAI/rexai/scores.txt", output)
+print(output)
